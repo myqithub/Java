ed9fc7fbba159eaad1103ded70850b2ac0624f2d
veysel dogan <veyselgithub.gmail.com>
2018-07-27 06:37:05 -0700

    Ilk Gonderme

---
+++ src/deneme1/Deneme1.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deneme1;
+
+/**
+ *
+ * @author Veyso
+ */
+public class Deneme1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           int sayi5=14,sayi6=53,sayi7;
+     double sayi8;
+        sayi8=sayi5+3.14;
+        System.out.println(sayi8);
+     
+     
+     
+     
+         System.out.println("Selam");
+         double pisayisi=3.14;
+         double yaricap=2.7896;
+         int alan;
+         
+          alan=(int)(yaricap*yaricap*pisayisi);
+         System.out.println("Alan:"+ alan); 
+         
+         
+         
+         String karakter1,karakter2,karakter3;
+         karakter1="A+l+i";
+         karakter2="Merhaba";
+         karakter3=karakter1+karakter2;
+         System.out.println(karakter3);
+         
+         
+         
+         
+         boolean tip=true;
+         System.out.println(tip);
+         if(tip){
+             System.out.println("Sonuc Dogru");
+    }
+         
+         
+         int sayi1 = 0,sayi2=571,sayi3=632;
+         byte kontrolSayisi=14,kapanisSayisi;
+         double a,b,c=12.99;
+         float d,e,f=14.53f;//f yazarsan tip değişimi yapar floata çevirir
+         char karakter='t';
+         System.out.println(sayi1+sayi2+sayi3);
+         System.out.println(c);
+        System.out.println(f);
+        System.out.println(karakter);
+        
+        
+    }
+    
+     
+     
+    
+  
+        
+}
